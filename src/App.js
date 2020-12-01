8853be3faa459c4cad82639d3018008445b48864
emersonpessoa01 <eps011108@gmail.com>
2020-12-01 19:55:11 -0300

    insert axios.create and axiosObject

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api-transaction-chanceller.herokuapp.com/",
+});
 
 export default function App() {
+  const [transactions, setTransactions] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchTransactions = async () => {
+      const axiosObject = await api.get("/transaction");
+
+      console.log(axiosObject);
+    };
+    fetchTransactions();
+  }, []);
+
   return (
-    <div>
-      <h1>Fala de dev!</h1>
+    <div className="containe">
+      <h1 className="center">Desafio Final do Bootcamp full Stack</h1>
     </div>
-  )
+  );
 }
