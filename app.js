7afcd1d4923c8a96e1f502f49ffb4eeedf7f1f05
emersonpessoa01 <eps011108@gmail.com>
2020-12-02 18:45:49 -0300

    adjust app of the frontend

--- app.js
+++ app.js
@@ -1,39 +1,66 @@
-import React from "react";
-import axios from "axios";
+import express from 'express'
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import path from "path";
+import cors from "cors";
+import { transactionRouter } from "./routes/transactionRouter.js";
+import dotenv from 'dotenv';
+dotenv.config();
 
-const api = axios.create({
-  baseURL: "https://api-transaction-chanceller.herokuapp.com/",
-  headers: {
-    'Content-type': 'application/json',
-  },
+//criando variaveis de ambiente
+// process.env.USER_DB = "emersonpessoa"
+
+//Conexao com o MongoDB
+console.log('Iniciando conexão ao MongoDB...');
+
+(async () => {
+  const { DB_CONNECTION } = process.env;
+  try {
+    await mongoose.connect(
+      DB_CONNECTION,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Conectado ao MongoDb Atlas");
+  } catch (err) {
+    console.log("Erro ao conectar no MongoDB");
+  }
+})();
+
+const app = express();
+// app.use(express.json());
+
+/**
+ * Vinculando o React ao app
+ */
+app.use(express.static(path.join('client/build')));
+
+/**
+ * lIBERANDO ACESSO À TODOS OS HOSTS
+ */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log("Acessou o Middleware!");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  app.use(cors());
+  next();
+});
+app.use("/transaction", transactionRouter);
+
+/**
+ * Rota raiz
+ */
+app.get('/api/', (_, res) => {
+  res.send({
+    message:
+      'Bem-vindo à API de lançamentos. Acesse /transaction e siga as orientações',
+  });
 });
 
-export default function App() {
-  const [transactions, setTransactions] = React.useState([]);
-
-  React.useEffect(() => {
-    const fetchTransactions = async () => {
-      // const axiosObject = await api.get("/transaction");//demonstra toda estrutura do objeto
-      const { data } = await api.get("/transaction"); //pegando somete o que interessa do vetor de objeto
-      // console.log(axiosObject);
-      console.log(data);
-
-      setTransactions(data)
-    };
-    fetchTransactions();
-  }, []);
-
-  return (
-    <div className="containe">
-      <h1 className="center">Desafio Final do Bootcamp full Stack</h1>
-
-      {
-        transactions.map(({_id,description}) =>{
-          return (
-          <p key={_id}>{description}</p>
-          )
-        })
-      }
-    </div>
-  );
-}
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Fala Dev -- APP startado na porta http://localhost:3002");
+});
--- client/src/App.js
+++ client/src/App.js
@@ -1,147 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import * as api from './api/ApiServices';
-import Spinner from './components/Spinnner';
-import GradesControl from './components/GradesControl';
-import FormPesquisa from './components/FormPesquisa';
-import ConuntSaldo from './components/ConuntSaldo';
-import ModalGrade from './components/ModalGrade';
-import CreateGrade from './components/CreateGrade';
+import React from "react";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api-transaction-chanceller.herokuapp.com/",
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
 
 export default function App() {
-  const [allGrades, setAllGrades] = useState([]);
-  const [yearMonth, setYearMonth] = useState('2020-07');
-  const [selectedGrade, setSelectedGrade] = useState({});
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [transactions, setTransactions] = React.useState([]);
 
-  useEffect(() => {
-    const getGrades = async () => {
-      const grades = await api.getAllGrades(yearMonth);
-      setAllGrades(grades);
+  React.useEffect(() => {
+    const fetchTransactions = async () => {
+      // const axiosObject = await api.get("/transaction");//demonstra toda estrutura do objeto
+      const { data } = await api.get("/transaction"); //pegando somete o que interessa do vetor de objeto
+      // console.log(axiosObject);
+      console.log(data);
+
+      setTransactions(data)
     };
-
-    getGrades();
-  }, [yearMonth]);
-
-  const handleActionAllGrades = (data) => {
-    setAllGrades(data);
-  };
-
-  const handleActionYearMonth = (data) => {
-    setYearMonth(data);
-  };
-
-  const handleClearGrades = () => {
-    setAllGrades([]);
-  };
-
-  const handleDelete = async (gradeToDelete) => {
-    const isDeleted = await api.deleteGrade(gradeToDelete);
-
-    if (isDeleted) {
-      const deletedGradeIndex = allGrades.findIndex(
-        (grade) => grade === gradeToDelete
-      );
-      const newGrades = Object.assign([], allGrades);
-      newGrades.splice(deletedGradeIndex, 1);
-      setAllGrades(newGrades);
-    }
-  };
-
-  const handlePersist = (grades) => {
-    setSelectedGrade(grades);
-
-    setIsModalOpen(true);
-  };
-
-  const handlePersistData = async (formData) => {
-    const { id } = formData;
-    const newGrades = Object.assign([], allGrades);
-
-    let gradeToPersist = null;
-
-    if (id !== undefined) {
-      gradeToPersist = newGrades.find(({ _id }) => _id === id);
-      gradeToPersist = formData;
-
-      const deletedGradeIndex = newGrades.findIndex(
-        (grade) => grade._id === id
-      );
-      newGrades.splice(deletedGradeIndex, 1);
-      await api.updateGrade(gradeToPersist);
-
-      newGrades.push(formData);
-    } else {
-      gradeToPersist = formData;
-      console.log(formData);
-
-      const valueInsert = await api.insertGrade(gradeToPersist);
-      newGrades.push(valueInsert);
-    }
-    setAllGrades(newGrades);
-    setIsModalOpen(false);
-  };
-
-  const handleClose = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleFilter = (filter) => {
-    const filterLowerCase = filter.toLowerCase();
-
-    const filtereLaçamenteos = allGrades.filter(({ description }) => {
-      const newDescription = description.toLowerCase();
-      return newDescription.includes(filterLowerCase);
-    });
-
-    setAllGrades(filtereLaçamenteos);
-  };
+    fetchTransactions();
+  }, []);
 
   return (
-    <div className="container" style={styles.containerBody}>
-      <div style={{ textAlign: 'center', fontWeight: 'bold' }}>
-        <h2> Bootcamp Desenvolvedor Full Stacks - Desafio Final</h2>
-        <h4>Controle Financeiro Pessoal</h4>
-      </div>
-      <hr />
-      {!isModalOpen && (
-        <FormPesquisa
-          yearMonth={yearMonth}
-          onPersist={handleActionAllGrades}
-          actionYearMonth={handleActionYearMonth}
-          clearGrades={handleClearGrades}
-        />
-      )}
-      <ConuntSaldo grades={allGrades} />
-      {!isModalOpen && (
-        <CreateGrade
-          onPersist={handlePersist}
-          grades={allGrades}
-          filter={handleFilter}
-        />
-      )}
+    <div className="containe">
+      <h1 className="center">Desafio Final do Bootcamp full Stack</h1>
 
-      {allGrades.length === 0 && <Spinner />}
-      {allGrades.length > 0 && (
-        <GradesControl
-          grades={allGrades}
-          onDelete={handleDelete}
-          onPersist={handlePersist}
-        />
-      )}
-      {isModalOpen && (
-        <ModalGrade
-          onSave={handlePersistData}
-          onClose={handleClose}
-          selectedGrade={selectedGrade}
-        />
-      )}
+      {
+        transactions.map(({_id,description}) =>{
+          return (
+          <p key={_id}>{description}</p>
+          )
+        })
+      }
     </div>
   );
 }
-
-const styles = {
-  containerBody: {
-    backgroundColor: 'white',
-    padding: '15px',
-  },
-};
