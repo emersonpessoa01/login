0daee994e6242ed4300a37464831a66f8c833004
emersonpessoa01 <eps011108@gmail.com>
2020-12-02 14:49:22 -0300

    redeploy

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import AddPokemon from './components/AddPokemon';
-import Pokemon from './components/Pokemon';
-import PokemonList from './components/PokemonList';
+const api = axios.create({
+  baseURL: "https://api-transaction-chanceller.herokuapp.com/",
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
 
-function App() {
+export default function App() {
+  const [transactions, setTransactions] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchTransactions = async () => {
+      // const axiosObject = await api.get("/transaction");//demonstra toda estrutura do objeto
+      const { data } = await api.get("/transaction"); //pegando somete o que interessa do vetor de objeto
+      // console.log(axiosObject);
+      console.log(data);
+
+      setTransactions(data)
+    };
+    fetchTransactions();
+  }, []);
+
   return (
-    <Router>
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <a href="/pokemon" className="navbar-brand">
-            Aplicativo
-          </a>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={'/pokemon'} className="nav-link">
-                Pokemons
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={'/add'} className="nav-link">
-                Add
-              </Link>
-            </li>
-          </div>
-        </nav>
-        <div className="container mt-3">
-          <Switch>
-            <Route exact path={['/', '/pokemon']} component={PokemonList} />
-            <Route exact path="/add" component={AddPokemon} />
-            <Route path="/pokemon/:id" component={Pokemon} />
-          </Switch>
-        </div>
-      </div>
-    </Router>
+    <div className="containe">
+      <h1 className="center">Desafio Final do Bootcamp full Stack</h1>
+
+      {
+        transactions.map(({_id,description}) =>{
+          return (
+          <p key={_id}>{description}</p>
+          )
+        })
+      }
+    </div>
   );
 }
-
-export default App;
