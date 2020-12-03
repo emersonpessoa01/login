e8ccd7c12bd86ffae6504b1526842cb8a11c0d01
emersonpessoa01 <eps011108@gmail.com>
2020-12-02 22:29:54 -0300

    changed app frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api-transaction-chanceller.herokuapp.com/",
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
 
 export default function App() {
-    return (
-      <div>Fala Dev!</div>
-    )
+  const [transactions, setTransactions] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchTransactions = async () => {
+      // const axiosObject = await api.get("/transaction");//demonstra toda estrutura do objeto
+      const { data } = await api.get("/transaction"); //pegando somete o que interessa do vetor de objeto
+      // console.log(axiosObject);
+      console.log(data);
+
+      setTransactions(data)
+    };
+    fetchTransactions();
+  }, []);
+
+  return (
+    <div className="containe">
+      <h1 className="center">Desafio Final do Bootcamp full Stack</h1>
+
+      {
+        transactions.map(({_id,description}) =>{
+          return (
+          <p className="center" key={_id}>{description}</p>
+          )
+        })
+      }
+    </div>
+  );
 }
