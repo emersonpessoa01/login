a6f051d9f33343832387f6703b36088c561cc045
emersonpessoa01 <eps011108@gmail.com>
2021-01-30 12:16:20 -0300

    updated 30/01 12:16

--- index.js
+++ index.js
@@ -37,10 +37,14 @@ app.get("/", (_, response) => {
  */
 app.use("/", routes);
 
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.join("client", "build", "index.html"));
-});
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'));
+    app.get('*',(req,res) =>{
+        res.sendFile(path.join(__dirname,"client","build","index.html"))
+    })
+}
+
 
 /**
  * Conexão ao Banco de Dados
