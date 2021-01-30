8d8a42ac574f7c1e91c3e75c87acc93a8d2446a5
emersonpessoa01 <eps011108@gmail.com>
2021-01-30 14:47:39 -0300

    updated 30/01 14:47

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ app.use("/", routes);
 
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static('client/build'));
-    app.get('*',(req,res) =>{
-        res.sendFile(path.join(__dirname,"client","build","index.html"))
-    })
+  app.use(express.static('client/build'));
+  app.get('*',(req,res) =>{
+      res.sendFile(path.join(__dirname,"client","build","index.html"))
+  })
 }
 
 
