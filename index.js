63fa6329102c5b0f5213952a138f35b6533a5435
emersonpessoa01 <eps011108@gmail.com>
2021-01-30 11:57:28 -0300

    updated 30/01 11:57

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("/", (_, response) => {
  * Rotas principais do app
  */
 app.use("/", routes);
-if (process.env.DB_CONNECTION && process.env.PORT === "production") {
+if (process.env.DB_CONNECTION === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
